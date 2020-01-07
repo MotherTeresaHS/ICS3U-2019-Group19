490c1c2dc3daac9f0c3e22c9b1ea7efcaee97ba5
jaeyoon-lee2 <54957190+jaeyoon-lee2@users.noreply.github.com>
2020-01-07 14:43:13 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# Created by: ????
-# Created on: ???? 2019
-# This file is the "????" game
+# Created by: Jaeyoon(Jay) Lee
+# Created on: Jan 2019
+# This file is the Avoid or Shoot game
 #   for CircuitPython
 
 import ugame
@@ -22,7 +22,7 @@ def blank_white_reset_scene():
 
     # set up the NeoPixels
     pixels = neopixel.NeoPixel(board.NEOPIXEL, 5, auto_write=False)
-    pixels.deinit() # and turn them all off
+    pixels.deinit()
 
     # reset sound to be off
     sound = ugame.audio
@@ -46,15 +46,9 @@ def blank_white_reset_scene():
 
     # repeat forever, game loop
     while True:
-        # get user input
-
-        # update game logic
-
-        # Wait for 1/2 seconds
         time.sleep(0.5)
         mt_splash_scene()
 
-        # redraw sprite list
 
 def mt_splash_scene():
     # this function is the MT splash scene
@@ -101,11 +95,6 @@ def mt_splash_scene():
     text1.text("MT Game Studios")
     text.append(text1)
 
-    text2 = stage.Text(width=29, height=14, font=None, palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
-    text2.move(35, 110)
-    text2.text("PRESS START")
-    text.append(text2)
-
     # get sound ready
     # follow this guide to convert your other sounds to something that will work
     #    https://learn.adafruit.com/microcontroller-compatible-audio-file-conversion
@@ -126,40 +115,164 @@ def mt_splash_scene():
 
     # repeat forever, game loop
     while True:
-        # get user input
-
-        # update game logic
-
-        # Wait for 1 seconds
         time.sleep(1.0)
-        game_splash_scene()
-
-        # redraw sprite list
-
-def game_splash_scene():
-    # this function is the game scene
-
-    # repeat forever, game loop
-    while True:
-        # get user input
-
-        # update game logic
-
-        # redraw sprite list
-        pass # just a placeholder until you write the code
+        main_menu_scene()
 
 
 def main_menu_scene():
-    # this function is the game scene
+    image_bank_3 = stage.Bank.from_bmp16("avoid_title.bmp")
 
-    # repeat forever, game loop
+    # sets the background to image 0 in the bank
+    background = stage.Grid(image_bank_3, constants.SCREEN_GRID_X,
+                            constants.SCREEN_GRID_Y)
+
+    # used this program to split the iamge into tile:
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 0)
+
+    background.tile(2, 3, 0)
+    background.tile(3, 3, 4)
+    background.tile(4, 3, 5)
+    background.tile(5, 3, 6)
+    background.tile(6, 3, 0)
+
+    background.tile(2, 4, 0)
+    background.tile(3, 4, 7)
+    background.tile(4, 4, 8)
+    background.tile(5, 4, 9)
+    background.tile(6, 4, 0)
+
+    # a list of sprites
+    sprites = []
+
+    # add text objects
+    text = []
+    text1 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text1.move(35, 100)
+    text1.text("START:START")
+    text.append(text1)
+
+    text2 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text2.move(35, 110)
+    text2.text("HELP:SELECT")
+    text.append(text2)
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = text + sprites + [background]
+    game.render_block()
+
     while True:
-        # get user input
+        keys = ugame.buttons.get_pressed()
 
-        # update game logic
+        if keys & ugame.K_START != 0:
+            selection_scene()
+        elif keys & ugame.K_SELECT != 0:
+            help_scene()
 
-        # redraw sprite list
-        pass # just a placeholder until you write the code
+        game.tick()
+
+
+def help_scene():
+    image_bank_2 = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    background = stage.Grid(image_bank_2, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+
+    text = []
+    text1 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text1.move(35, 10)
+    text1.text("HOW TO PLAY")
+    text.append(text1)
+
+    text2 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text2.move(35, 30)
+    text2.text("Use D-Pad to fly")
+    text.append(text2)
+
+    text3 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text3.move(35, 40)
+    text3.text("SHOOT [A]")
+    text.append(text3)
+
+    text4 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text4.move(35, 70)
+    text4.text("SOUND [B]")
+    text.append(text4)
+
+    text5 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text5.move(35, 80)
+    text5.text("START:START")
+    text.append(text5)
+
+    text6 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text6.move(35, 100)
+    text6.text("CREATED BY JAY")
+    text.append(text6)
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = text + [background]
+    game.render_block()
+
+    while True:
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_START != 0:
+            main_menu_scene()
+
+        game.tick()
+
+
+def selection_scene():
+    # this function is the air plane selection scene
+    image_bank_1 = stage.Bank.from_bmp16("avoid_shoot.bmp")
+
+    background = stage.Grid(image_bank_1, constants.SCREEN_GRID_X,
+                            constants.SCREEN_GRID_Y)
+
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 1)
+            background.tile(x_location, y_location, tile_picked)
+
+    text = []
+
+    text1 = stage.Text(width=29, height=14, font=None,
+                       palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text1.move(20, 10)
+    text1.text("PLANE SELECTION")
+    text.append(text1)
+
+    sprites = []
+
+    plane1 = stage.Sprite(image_bank_1, 6, int(constants.SCREEN_X / 4),
+                        int(constants.SCREEN_Y / 4))
+    sprites.append(plane1)
+
+    game = stage.Stage(ugame.display, constants.FPS)
+    game.layers = sprites + text + [background]
+    game.render_block()
+
+    time.sleep(1.0)
+    while True:
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_SELECT != 0:
+            print("here")
+        elif keys & ugame.K_START != 0:
+            game_scene(plane_info)
+
+        game.tick()
 
 
 def game_scene():
